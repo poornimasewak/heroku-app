6fb5cdcb0c11690996bb11861cb4171dca4358c3
poornimasewak <poornima.mathur.20@gmail.com>
2020-04-24 10:50:57 -0500

    added server.js, index.html, style.css files

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var PORT = process.env.PORT || 8080;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+})
+app.listen(PORT, function () {
+    console.log('Server listening on: http://localhost:' + PORT);
+});
